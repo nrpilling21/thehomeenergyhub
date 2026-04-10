7157e00f2031fb9af34bf7643328334a717697ca
nrpilling21 <nick@studionick.co.uk>
2026-04-10 11:53:41 +0100

    audit: fix robots.ts sitemap URL to use www prefix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/api/'],
       },
     ],
-    sitemap: 'https://thehomeenergyhub.co.uk/sitemap.xml',
+    sitemap: 'https://www.thehomeenergyhub.co.uk/sitemap.xml',
   };
 }
